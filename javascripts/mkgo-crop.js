17b56b094f73b4e422e9e00a0d043a468c512725
kuangjiajia <1298201366@qq.com>
2017-11-21 20:02:18 +0800

    asdsad

--- javascripts/mkgo-crop.js
+++ javascripts/mkgo-crop.js
@@ -130,7 +130,7 @@ function initMkgoCrop(mkCrop) {
 	clearRect(cCObj.c2d, cCObj.cas.x, cCObj.cas.y, cxyobj.zleng, cxyobj.zleng);
 	//给裁剪掉的区域画一个边框
 	strokeRect(cCObj.c2d, {'strokeStyle': '#FFFFFF'},
-		cCObj.cas.x, cCObj.cas.y, cxyobj.zleng, cxyobj.zleng);
+		0, 0, cxyobj.zleng, cxyobj.zleng);
 
 	//加载待裁剪的图像
 	CropImage.initImage(bCObj.c2d, mkCrop.imgsrc);
