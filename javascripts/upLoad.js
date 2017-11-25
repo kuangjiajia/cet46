d488a1495dbc62825cbf50d4458d65d76b9a24ed
kuangjiajia <1298201366@qq.com>
2017-11-25 15:53:03 +0800

    asd

--- javascripts/upLoad.js
+++ javascripts/upLoad.js
@@ -150,11 +150,11 @@
             data: dt,
             success: function(res) {
                 console.log(res);
+                alert(JSON.stringify(res))
             },
             error: function(res) {
                 console.log(res);
             },
-            // contentType: "application/json; charset=utf-8",
             dataType: "json"
         });
     })
