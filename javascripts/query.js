a80f4451f815bbabab73fa1c54b5484d62b29565
kuangjiajia <1298201366@qq.com>
2017-11-25 15:09:18 +0800

    asd

--- javascripts/query.js
+++ javascripts/query.js
@@ -6,25 +6,26 @@ let dt = {
     "openid": openid,
     "type": 1
 }
-alert(openid);
-$.ajax({
-    type: 'POST',
-    url: "/UserData.php",
-    data:dt,
-    success: function(res) {
-        // console.log(res);
-        // let data = res.data;
-        // console.log(data);
-        // let cet_name = data.name;
-        // let cet_id = data.examID;
-        // stu_name.value = cet_name;
-        // stu_card.value = cet_id;
-        console.log(res);
-        // alert(openid);
-    },
-    error: function(res) {
-        console.log(res);
-    },
-    // contentType: "application/json; charset=utf-8",
-    dataType: "json"
-});
+alert(JOSN.stringify(search));
+// alert(openid);
+// $.ajax({
+//     type: 'POST',
+//     url: "/UserData.php",
+//     data:dt,
+//     success: function(res) {
+//         // console.log(res);
+//         // let data = res.data;
+//         // console.log(data);
+//         // let cet_name = data.name;
+//         // let cet_id = data.examID;
+//         // stu_name.value = cet_name;
+//         // stu_card.value = cet_id;
+//         console.log(res);
+//         // alert(openid);
+//     },
+//     error: function(res) {
+//         console.log(res);
+//     },
+//     // contentType: "application/json; charset=utf-8",
+//     dataType: "json"
+// });
