249276f60f5bc8833e406dda6191fa35e73feabd
kuangjiajia <1298201366@qq.com>
2017-11-26 22:37:28 +0800

    sad

--- javascripts/aboutMe.js
+++ javascripts/aboutMe.js
@@ -1,18 +1,21 @@
 
-let nowUrl =  decodeURI(window.location.search);
-function param(url) {
-    const index = url.indexOf("?");
-    let str = url.slice(index+1);
-    let json = str.split("&");
-    let obj = {};
-    for(var i = 0 ; i < json.length; i++){
-        obj[json[i].slice(0,json[i].indexOf("="))] = json[i].slice(json[i].indexOf("=")+1);
-    }
-    return obj;
-}
+let nowUrl = decodeURIComponent(window.location.search);
+alert(nowUrl);
+let nUrl = decodeURI(window.location.search);
+alert(nUrl);
+// function param(url) {
+//     const index = url.indexOf("?");
+//     let str = url.slice(index+1);
+//     let json = str.split("&");
+//     let obj = {};
+//     for(var i = 0 ; i < json.length; i++){
+//         obj[json[i].slice(0,json[i].indexOf("="))] = json[i].slice(json[i].indexOf("=")+1);
+//     }
+//     return obj;
+// }
 
-let message = param(nowUrl);
-alert(JSON.stringify(message));
+// let message = param(nowUrl);
+// alert(JSON.stringify(message));
 
 // $.ajax({
 //     type: 'GET',
