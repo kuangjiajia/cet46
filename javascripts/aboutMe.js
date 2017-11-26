4ed4d764c01b6d8f546b7923d8ad26db91e8b79c
kuangjiajia <1298201366@qq.com>
2017-11-26 22:39:09 +0800

    sad

--- javascripts/aboutMe.js
+++ javascripts/aboutMe.js
@@ -1,21 +1,27 @@
 
 let nowUrl = decodeURIComponent(window.location.search);
 alert(nowUrl);
-let nUrl = decodeURI(window.location.search);
-alert(nUrl);
-// function param(url) {
-//     const index = url.indexOf("?");
-//     let str = url.slice(index+1);
-//     let json = str.split("&");
-//     let obj = {};
-//     for(var i = 0 ; i < json.length; i++){
-//         obj[json[i].slice(0,json[i].indexOf("="))] = json[i].slice(json[i].indexOf("=")+1);
-//     }
-//     return obj;
-// }
+function param(url) {
+    const index = url.indexOf("?");
+    let str = url.slice(index+1);
+    let json = str.split("&");
+    let obj = {};
+    for(var i = 0 ; i < json.length; i++){
+        obj[json[i].slice(0,json[i].indexOf("="))] = json[i].slice(json[i].indexOf("=")+1);
+    }
+    return obj;
+}
 
-// let message = param(nowUrl);
+let message = param(nowUrl);
 // alert(JSON.stringify(message));
+
+var stu_name = document.querySelector(".name");
+//证号
+var stu_card = document.querySelector(".card");
+let cet_name = data.name;
+let cet_id = data.examID;
+stu_name.value = cet_name;
+stu_card.value = cet_id;
 
 // $.ajax({
 //     type: 'GET',
