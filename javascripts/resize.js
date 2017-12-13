e0aff1d5e0ffdd9e9fdfa3829f08ca0b87085b6d
kuangjiajia <1298201366@qq.com>
2017-12-14 00:37:03 +0800

    sa

--- javascripts/resize.js
+++ javascripts/resize.js
@@ -8,3 +8,12 @@ $(window).resize(function() {
         $('footer').show();
     }
 });
+
+$(".warn").on("click",function() {
+  console.log($(".remind_mes").css("display"))
+  $(".remind_mes").css("display","block");
+})
+
+$(".remind_mes").on("click",function() {
+  $(".remind_mes").css("display","none");
+})
