38938099bfac27a2d0209629a68d211667d5d7e7
kuangjiajia <1298201366@qq.com>
2017-12-17 12:55:27 +0800

    asdas

--- javascripts/resize.js
+++ javascripts/resize.js
@@ -8,12 +8,3 @@ $(window).resize(function() {
         $('footer').show();
     }
 });
-
-$(".warn").on("click",function() {
-  console.log($(".remind_mes").css("display"))
-  $(".remind_mes").css("display","block");
-})
-
-$(".remind_mes").on("click",function() {
-  $(".remind_mes").css("display","none");
-})
