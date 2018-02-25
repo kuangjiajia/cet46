b7293ccbdb3a686fc9e4e51621242d3e70fe80bc
kuangjiajia <1298201366@qq.com>
2018-02-25 13:41:42 +0800

    I Change the way to url

--- javascripts/query.js
+++ javascripts/query.js
@@ -10,4 +10,22 @@ let dt = {
 
 
 
+function paramsTojson(url) {
+    var json = {}
+    var params = url.split("?")[1]
+    var props = params.split("&")
+    for(var i = 0; i < props.length; i++) {
+        var tmp = props[i].split("=")
+        json[tmp[0]] = tmp[1]
+    }
+    return json
+}
+
+var json = paramsTojson(window.location.url)
+$(".name").innerHTML = json['name']
+$(".card").innerHTML = json['card']
+$(".total-point").innerHTML = json['sumScore']
+$(".listen-point").innerHTML = json['listening']
+$(".read-type").innerHTML = json['reading']
+$(".write-point").innerHTML = json['writing']
 
