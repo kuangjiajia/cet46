7732bbb3c0def53a1e755e3c754443df63546b25
kuangjiajia <1298201366@qq.com>
2018-02-25 13:49:39 +0800

    I Change the way to url

--- javascripts/query.js
+++ javascripts/query.js
@@ -1,34 +1,22 @@
-let search = window.location.search;
-let index = search.indexOf("=");
-let openid = search.substr(index+1);
-let maxsize = 4*1024*1024;//2M 
-let dt = {
-    "openid": openid,
-    "type": 1
-}
-
-window.onload = function() {
-    function paramsTojson(url) {
-        var json = {}
-        var params = url.split("?")[1]
-        var props = params.split("&")
-        for(var i = 0; i < props.length; i++) {
-            var tmp = props[i].split("=")
-            json[tmp[0]] = tmp[1]
-        }
-        return json
+function paramsTojson(url) {
+    var json = {}
+    var params = url.split("?")[1]
+    var props = params.split("&")
+    for(var i = 0; i < props.length; i++) {
+        var tmp = props[i].split("=")
+        json[tmp[0]] = tmp[1]
     }
-    
-    var json = paramsTojson(window.location.href)
-    $(".name").innerHTML = json['name']
-    $(".card").innerHTML = json['card']
-    $(".total-point").innerHTML = json['sumScore']
-    $(".listen-point").innerHTML = json['listening']
-    $(".read-type").innerHTML = json['reading']
-    $(".write-point").innerHTML = json['writing']
-    
-    
-    
-    console.log(json)
+    return json
 }
 
+var json = paramsTojson(window.location.href)
+$(".name").innerHTML = json['name']
+$(".card").innerHTML = json['card']
+$(".total-point").innerHTML = json['sumScore']
+$(".listen-point").innerHTML = json['listening']
+$(".read-type").innerHTML = json['reading']
+$(".write-point").innerHTML = json['writing']
+
+
+
+console.log(json)
